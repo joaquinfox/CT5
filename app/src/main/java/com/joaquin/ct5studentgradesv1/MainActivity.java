9a47ba3da167aec549dde441263865e4ca7062d2
Jimmy <joaquin.fox@gmail.com>
2022-08-13 21:13:49 -0700

    first commit

---
+++ app/src/main/java/com/joaquin/ct5studentgradesv1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.joaquin.ct5studentgradesv1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
