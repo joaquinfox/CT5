ac6cf057c916722b3b0534d4158bf58d95b7ca97
Jimmy <joaquin.fox@gmail.com>
2022-08-15 20:02:40 -0700

    addStarterData,addStudent

--- app/src/main/java/com/joaquin/ct5studentgradesv1/repo/GradeRepository.java
+++ app/src/main/java/com/joaquin/ct5studentgradesv1/repo/GradeRepository.java
@@ -1,4 +1,40 @@
 package com.joaquin.ct5studentgradesv1.repo;
 
+import android.content.Context;
+
+import com.joaquin.ct5studentgradesv1.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GradeRepository {
+    private static GradeRepository gradeRepo;
+    private final List<Student> studentList;
+
+
+
+    // Instantiate a single instance???
+    public static GradeRepository getInstance(Context context) {
+        if (gradeRepo == null) {
+            gradeRepo = new GradeRepository(context);
+        }
+        return gradeRepo;
+    }
+    private GradeRepository(Context context){
+        studentList = new ArrayList<>();
+        addStarterData();
+    }
+
+    private void addStarterData() {
+        // Add some students
+        Student student = new Student("Chuck","Finkleton", 60.20);
+        student.setId(1);
+        addStudent(student);
+    }
+
+    private void addStudent(Student student) {
+        studentList.add(student);
+    }
+
+
 }
