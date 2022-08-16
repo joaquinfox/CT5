3956e2ae70e99c78ecc5a656a2314342f077d4b9
Jimmy <joaquin.fox@gmail.com>
2022-08-15 20:46:19 -0700

    wrote StudentListViewModel

--- app/src/main/java/com/joaquin/ct5studentgradesv1/viewmodel/StudentListViewModel.java
+++ app/src/main/java/com/joaquin/ct5studentgradesv1/viewmodel/StudentListViewModel.java
@@ -1,4 +1,24 @@
 package com.joaquin.ct5studentgradesv1.viewmodel;
 
+import android.app.Application;
+
+import com.joaquin.ct5studentgradesv1.model.Student;
+import com.joaquin.ct5studentgradesv1.repo.GradeRepository;
+
+import java.util.List;
+
 public class StudentListViewModel {
+    private GradeRepository gradeRepo;
+
+    public StudentListViewModel(Application application) {
+        gradeRepo = GradeRepository.getInstance(application.getApplicationContext());
+    }
+
+    public List<Student> getStudents() {
+        return gradeRepo.getStudents();
+    }
+
+    public void addStudent(Student student){
+        gradeRepo.addStudent(student);
+    }
 }
