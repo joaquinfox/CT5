c8acf3d433da4e5fa31f8528797debf064ea7297
Jimmy <joaquin.fox@gmail.com>
2022-08-15 20:11:15 -0700

    more GradeRepository

--- app/src/main/java/com/joaquin/ct5studentgradesv1/repo/GradeRepository.java
+++ app/src/main/java/com/joaquin/ct5studentgradesv1/repo/GradeRepository.java
@@ -12,7 +12,6 @@ public class GradeRepository {
     private final List<Student> studentList;
 
 
-
     // Instantiate a single instance???
     public static GradeRepository getInstance(Context context) {
         if (gradeRepo == null) {
@@ -20,20 +19,43 @@ public class GradeRepository {
         }
         return gradeRepo;
     }
-    private GradeRepository(Context context){
+
+    private GradeRepository(Context context) {
         studentList = new ArrayList<>();
         addStarterData();
     }
 
     private void addStarterData() {
         // Add some students
-        Student student = new Student("Chuck","Finkleton", 60.20);
+        Student student = new Student("Chuck", "Finkleton", 60.20);
+        student.setId(1);
+        addStudent(student);
+        student = new Student("Nancy", "Bingbing", 60.20);
+        student.setId(1);
+        addStudent(student);
+        student = new Student("Frank", "Wooper", 60.20);
+        student.setId(1);
+        addStudent(student);
+        student = new Student("Tanya", "Barker", 60.20);
         student.setId(1);
         addStudent(student);
     }
 
-    private void addStudent(Student student) {
+    public void addStudent(Student student) {
         studentList.add(student);
+    }
+
+    public Student getStudent(long studentId) {
+        for (Student student : studentList) {
+            if (student.getId() == studentId) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public List<Student> getStudents() {
+        return studentList;
     }
 
 
